7ff166e15a95c5653f459aee1f5dc0fb7be179e0
Alex Tetervak <tetervak@sheridancollege.ca>
2026-02-19 13:49:01 -0500

    one die works

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { OneDice } from "./components/OneDice";
+import { getRandomDiceValue } from "./data/dice-data";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [diceValue, setDiceValue] = useState(1);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const onRollDice = () => {
+        setDiceValue(getRandomDiceValue());
+    };
+
+    const onNextDice = () => {
+        let nextValue: number;
+        if (diceValue < 6) {
+            nextValue = diceValue + 1;
+        } else {
+            nextValue = 1;
+        }
+        setDiceValue(nextValue);
+    };
+
+    return (
+        <>
+            <h1>Dice Game</h1>
+            <p><OneDice side={diceValue} /></p>
+            <p><button onClick={onRollDice}>Roll Dice</button></p>
+            <p><button onClick={onNextDice}>Next Dice</button></p>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/data/dice-data.tsx
@@ -0,0 +1,3 @@
+export function getRandomDiceValue(): number {
+  return 1 + Math.floor(6 * Math.random());
+}
